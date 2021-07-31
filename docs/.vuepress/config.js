770255b449ef97769fee5a2c04df1d96b74a548c
hongcheng.fu <hongcheng.fu@jniu.com>
2021-07-31 18:11:28 +0800

    [Add] 添加markdown 流程图支持

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,7 @@ module.exports = {
     ["vuepress-plugin-reading-progress"], //顶部进度条
     ["go-top"], // 悬挂猫返回顶部,yarn add -D vuepress-plugin-go-top
     ["element-ui"], // element-ui 插件
-    ["flexsearch-pro"]
+    ["flexsearch-pro"], // 搜索支持
+    ["vuepress-plugin-mermaidjs"], // mermaid 支持 流程图
   ]
 }
